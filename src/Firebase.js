c5f09c1800b0d1a6dc5b5b1ec4d86067b209743e
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2022-11-30 17:30:25 +0100

    Added firebase and reactfire modules

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default firebaseConfig = {
+    apiKey: "AIzaSyCr1X-q56d5qTPFzSmk7npHfURS4AUVKQU",
+    authDomain: "spritegames-77777.firebaseapp.com",
+    databaseURL: "https://spritegames-77777-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "spritegames-77777",
+    storageBucket: "spritegames-77777.appspot.com",
+    messagingSenderId: "919053433947",
+    appId: "1:919053433947:web:9d6617ae820d200801d37e",
+    measurementId: "G-FS0QN26Y8Y"
+  };
