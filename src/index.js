f92acf361e0500cbeff651ffd535279655caefb6
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2022-12-26 02:23:13 +0100

    Added pages implementation

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import "./index.css";
 import App from "./App";
 import Game from "./Game";
 import Login from "./Login";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import SingUp from "./SignUp";
-import firebaseConfig from "./firebaseConfig";
 
 export default function AppGlobal() {
   
@@ -16,7 +14,8 @@ export default function AppGlobal() {
     // <FirebaseAppProvider firebaseConfig={firebaseConfig} >
     //   <Suspense fallback={<p>Loading...</p>}> {/*If firebase needs to load it will show this fallback*/}
         // <HashRouter basename={process.env.PUBLIC_URL}>
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        // I'm using HashRouter since using BrowseRouter won't work when deploying to GitHub pages, besides that i made some changes to the URL's in order to work for the name of the repository on the URL of the deployed wen
+        <HashRouter>
           <Routes>
             <Route exact path="/" element={<App/>} />
             <Route exact path="/game" element={<Game/>} />
@@ -33,9 +32,6 @@ export default function AppGlobal() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppGlobal />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
 //   <React.StrictMode>
